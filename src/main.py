82d110ee289ef433023d875da022f78441a556aa
racheldmiller <rachelmiller415@gmail.com>
2020-08-18 08:39:55 -0700

    progress

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 
-print (pygame.ver)
+print(pygame.ver)
+
 
 '''
 The potential idea is to use "states" - when it's the X-person's turn to go, we're in X state, and vice versa. It might be a little extra, but it helps organize a little bit at least. 
@@ -14,5 +15,4 @@ Possible methods within a state:
 -checkForWin
 -addMark (interact with GUI to add X or O)
 -retrieveBoard (see which spots are open...? Not sure how we can relay that to backend)
-
 '''
--- src/states/main.py
+++ src/states/main.py
@@ -5,37 +5,38 @@ name2 = input("Please enter the name of player 2: ")
 
 # rand_num = randint(0,9)
 
-print (name1 + " will be X's")
-print (name2 + " will be O's")
+print(name1 + " will be X's")
+print(name2 + " will be O's")
 
-end = False 
+end = False
 
-while end == False :
+while end == False:
 
-    matrix = [" ", " "," ", " "," ", " "," ", " ", " "]
+    matrix = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
     print("_________________________ \n \n")
     print(matrix[0] + "|" + matrix[1] + "|" + matrix[2])
-    #print()
-    #print()
+    # print()
+    # print()
     print("________")
     print(matrix[3] + "|" + matrix[4] + "|" + matrix[5])
     print("________")
     print(matrix[6] + "|" + matrix[7] + "|" + matrix[8])
-    #if matrix matches any of the winnable combinations OR we fill the board, set end to True
+    # if matrix matches any of the winnable combinations OR we fill the board, set end to True
     end = True
 
+# POSSIBLE COMBINATIONS
 
-#vertical
+# vertical
 # (0,3,6)
 # (1,4,7)
 # (2,5,8)
 
-#horizontal
-#(0,1,2)
-#(3,4,5)
-#(6,7,8)
+# horizontal
+# (0,1,2)
+# (3,4,5)
+# (6,7,8)
 
-#diagonal
+# diagonal
 # (0,4,8)
 # (2,4,6)
